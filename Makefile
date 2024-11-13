a8d32e558c949740659fa284ed831792ca9b162c
Antoni Gorski <agorski@c3r7s4.42warsaw.pl>
2024-11-13 15:45:45 +0100

    readdline implementation

--- Makefile
+++ Makefile
@@ -6,7 +6,12 @@
 #    By: agorski <agorski@student.42.fr>            +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2024/11/13 14:45:11 by agorski           #+#    #+#              #
-#    Updated: 2024/11/13 14:45:12 by agorski          ###   ########.fr        #
+#    Updated: 2024/11/13 15:45:11 by agorski          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
+# to compile the minishell
+#cc minishell.c -lreadline -o minishell
+
+# to check for memory leaks without readline
+# valgrind --leak-check=full --suppressions=../writing_helpers/valgrind_ignore.md ./minishell
--- c_files/minishell.c
+++ c_files/minishell.c
@@ -6,7 +6,7 @@
 /*   By: agorski <agorski@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/13 14:33:16 by agorski           #+#    #+#             */
-/*   Updated: 2024/11/13 15:14:48 by agorski          ###   ########.fr       */
+/*   Updated: 2024/11/13 15:36:33 by agorski          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,17 +17,16 @@ int main(int argc, char **argv)
     while (1)
     {
         char *line;
-        line = readline("minishell$ ");
-        if (!line)
+        line = readline("antpaw$"); // Wyświetla prompt "antpaw$" i czeka na wprowadzenie danych
+        if (!line) // Sprawdza, czy użytkownik wprowadził EOF
             break;
-        if (*line)
-            add_history(line);
-        printf("%s\n", line);
-        free(line);
+        if (*line) // Sprawdza, czy linia nie jest pusta
+            add_history(line); // Dodaje linię do historii
+        printf("%s\n", line); // Wyświetla wprowadzone dane
+        free(line); // Zwalnia pamięć przydzieloną przez readline
     }
 }
 //główna funkcja programu, w której wywoływana jest funkcja readline,
 //która zwraca wskaźnik na bufor z wczytaną linią
 //jeżeli wczytana linia nie jest pusta, to dodajemy ją do historii
 //i wypisujemy na ekranie
-
