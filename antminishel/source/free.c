e7e5e309d306b087fb1294891242cb039a6e96a7
Antoni Gorski <agorski@c1r8s4.42warsaw.pl>
2024-12-18 11:09:53 +0100

    free function shorter no duplicate code

--- antminishel/source/free.c
+++ antminishel/source/free.c
@@ -6,7 +6,7 @@
 /*   By: agorski <agorski@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/16 12:23:49 by agorski           #+#    #+#             */
-/*   Updated: 2024/12/17 12:35:24 by agorski          ###   ########.fr       */
+/*   Updated: 2024/12/18 10:52:35 by agorski          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,7 @@
 
 void	ft_shell_free(t_minishell *minishell)
 {
-	int	i;
-
-	i = 0;
-	while (minishell->lexter_tab[i])
-	{
-		free(minishell->lexter_tab[i]);
-		i++;
-	}
-	free(minishell->lexter_tab);
+	tab_free(minishell->lexter_tab);
 	free(minishell->line);
 }
 
