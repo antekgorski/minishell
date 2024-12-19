5479049bb43202e82fad27b5ff13763555e3dbe4
Antoni Gorski <agorski@c2r1s4.42warsaw.pl>
2024-12-19 13:19:02 +0100

    list env in table tested

--- antminishel/.test/env_list_test.c
+++ antminishel/.test/env_list_test.c
@@ -1,6 +1,6 @@
 #include "minishell.h"
 
-//cc env_list_test.c ../source/env.c -I../headers -L ../libft -lft -g
+// cc env_list_test.c ../source/env.c -I../headers -L ../libft -lft -g
 
 void	minishell_init(t_minishell *minishell, char **envp)
 {
@@ -10,17 +10,40 @@ void	minishell_init(t_minishell *minishell, char **envp)
 	env_start(minishell, envp);
 }
 
+// print env list
+void	print_env(t_minishell *minishell)
+{
+    //ptint one env variable
+    printf("%s\n%s\n", ((char **)minishell->m_env->content)[0], ((char **)minishell->m_env->content)[1]);
+	
+    
+    //print all env variables
+    // t_list	*current;
+	// char	**env_vars;
+
+	// current = minishell->m_env;
+
+
+	// while (current != NULL)
+	// {
+	// 	env_vars = (char **)current->content;
+	// 	if (env_vars != NULL)
+	// 	{
+	// 		for (int i = 0; env_vars[i] != NULL; i++)
+	// 		{
+	// 			printf("env_vars[%d]: %s\n", i, env_vars[i]);
+	// 		}
+	// 	}
+	// 	current = current->next;
+	// }
+}
+
 int	main(int argc, char **argv, char **envp)
 {
-    (void)argc;
-    (void)argv;
-    t_minishell minishell;
-    minishell_init(&minishell, envp);
-    while(minishell.m_env)
-    {
-        printf("%s\n", (char *)minishell.m_env->content);
-        minishell.m_env = minishell.m_env->next;
-    }
-    
-    return 0;
+	(void)argc;
+	(void)argv;
+	t_minishell minishell;
+	minishell_init(&minishell, envp);
+	print_env(&minishell);
+	return (0);
 }
