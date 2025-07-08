e120f4f59a19a233b753c6753d34ea39da9bd518
antekgorski <antek.gorski@wp.pl>
2025-07-08 14:06:44 +0200

    tes No_2
    this is No_2 test for coauthor
    
    Co-Authored-By: Paweł Rutkowski <95161699+prutkows42@users.noreply.github.com>

--- headers/minishell.h
+++ headers/minishell.h
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-//Test
+//Test No_2
 //Co-authored-by: Pawel Rudkowski <pawa202@wp.pl>"   
 //Co-authored-by: Antek Gorski <antek.gorski@wp.pl>"   
 
